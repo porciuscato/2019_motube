1a43a516dc1224c3a40b847294d95dc1b1c392cc
lhbbbb <lhb4196@gmail.com>
2019-11-27 17:00:51 +0900

    modify profile

--- motube/settings.py
+++ motube/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/motube/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
--- movies/views.py
+++ movies/views.py
@@ -15,6 +15,7 @@ def index(request):
     }
     return render(request, 'movies/index.html', context)
 
+@login_required
 def detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     reviews = movie.review_set.all()
