71db4009b423058cf62623c0571e88a8dd6e75bf
unknown <mpcato>
2019-11-29 03:13:55 +0900

    modify settings

--- motube/settings.py
+++ motube/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://ssafy-create.herokuapp.com",
+]
 
 
 # Application definition
