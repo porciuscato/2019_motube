d3b5345c1daabbe9b1b0b539a2c51becc1091d4d
lhbbbb <lhb4196@gmail.com>
2019-11-28 19:17:21 +0900

    complete recommend algorithm

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from movies.models import Movie, Movie_Score
 from django.shortcuts import render, redirect, get_object_or_404, render_to_response
 from django.contrib.auth import get_user_model
 from .forms import CustomUserCreationForm
@@ -7,6 +10,57 @@ from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from movies.models import Review
 from IPython import embed
+from django_pandas.io import read_frame
+import math
+
+def sim_pearson(data, name1, name2):
+    sumX=0 # X의 합
+    sumY=0 # Y의 합
+    sumPowX=0 # X 제곱의 합
+    sumPowY=0 # Y 제곱의 합
+    sumXY=0 # X*Y의 합
+    count=0 #영화 개수
+    for i in data[name1]: # i = key
+        if math.isnan(data[name1][i]):
+            continue
+        if math.isnan(data[name2][i]):
+            continue
+        # name1, name2 둘 다 관심있는 영화
+        sumX+=data[name1][i]
+        sumY+=data[name2][i]
+        sumPowX+=pow(data[name1][i],2)
+        sumPowY+=pow(data[name2][i],2)
+        sumXY+=data[name1][i]*data[name2][i]
+        count+=1
+        
+    a = sumXY- ((sumX*sumY)/count)
+    b = (sumPowX - (pow(sumX,2) / count))
+    c = (sumPowY - (pow(sumY,2) / count))
+    d = math.sqrt(b * c)
+    if d == 0:
+        return 0
+    return a / d
+
+def top_match(data, name, index=3):
+    li=[]
+    for i in data: #딕셔너리를 돌고
+        if name!=i: #자기 자신이 아닐때만
+            li.append((sim_pearson(data,name,i),i)) #sim_function()을 통해 상관계수를 구하고 li[]에 추가
+    li.sort() #오름차순
+    li.reverse() #내림차순
+    return li[:index]
+
+def get_recommend(data, person):
+    res = top_match(data, person)
+    sim_person = max(res)[1]
+    lst = []
+    for movie in data[sim_person]:
+        if math.isnan(data[sim_person][movie]):
+            continue
+        if math.isnan(data[person][movie]):
+            lst.append(movie)
+
+    return lst[:4]
 
 # Create your views here.
 def index(request):
@@ -16,16 +70,23 @@ def index(request):
     }
     return render(request, 'accounts/index.html', context=context)
 
-
-
 def user_detail(request, user_pk):
     person = get_object_or_404(get_user_model(), pk=user_pk)
     like_movies = person.like_movies.all()
     reviews = Review.objects.filter(author=user_pk)
+    scores = Movie_Score.objects.all()
+    ratings = scores.to_pivot_table(values='score', rows='movie', cols='user').to_dict()
+    movie_names = get_recommend(ratings, request.user.username)
+    movie_lst = []
+    for name in movie_names:
+        movie = Movie.objects.filter(title=name)[0]
+        movie_lst.append(movie)
+
     context = {
         'person': person,
         'reviews': reviews,
         'like_movies': like_movies,
+        "reco_movies": movie_lst
     }
     return render(request, 'accounts/detail.html', context=context)
 
